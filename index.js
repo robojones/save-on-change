3f7fb68e524bcbb89fd1c1757d019813f4c5c517
robojones <jonathan.scholz-weil@t-online.de>
2017-01-18 16:55:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const fs = require('fs')
+const path = require('path')
+
+const cache = {}
+
+function autoSave(filename, onSave) {
+  const filepath = path.resolve(filename)
+
+  if(filepath in cache) {
+    return cache[filepath]
+  }
+
+  const object = read(filepath)
+  let changed = false
+  let writing = false
+
+  return cache[filepath] = wrap(object, change)
+
+  function change() {
+    if(!changed) {
+      changed = true
+
+      if(!writing) {
+        process.nextTick(save)
+      }
+    }
+  }
+
+  function save() {
+    changed = false
+    writing = true
+
+    const json = JSON.stringify(object)
+
+    if(typeof onSave === 'function') {
+      fs.writeFile(filepath, json, () => {
+
+        onSave(cache[filepath])
+
+        if(changed) {
+          process.nextTick(save)
+        } else {
+          writing = false
+        }
+      })
+    } else {
+      fs.writeFileSync(filepath, json)
+    }
+  }
+}
+
+function wrap(o, change) {
+  const innerCache = {}
+
+  return new Proxy(o, {
+    set(o, prop, v) {
+      if(typeof o === 'object') {
+
+        if(innerCache[prop] !== v) {
+          change()
+        }
+
+      } else if(v !== o[prop]) {
+        change()
+      }
+
+      o[prop] = v
+    },
+    get(o, prop) {
+      if(typeof o[prop] === 'object') {
+        if(prop in innerCache) {
+          return innerCache[prop]
+        } else {
+          return innerCache[prop] = wrap(o[prop])
+        }
+      } else {
+        return o[prop]
+      }
+    }
+  })
+}
+
+function read(p) {
+  try {
+    const raw = fs.readFileSync(p)
+    return JSON.parse(raw)
+  } catch(err) {
+    return {}
+  }
+}
+
+module.exports = autoSave
---
+++ test/index.js
@@ -0,0 +1,78 @@
+const assert = require('assert')
+const fs = require('fs')
+const functs = require('functs')
+
+const FILE = 'example.json'
+
+describe('autoSave(filename[, onSave])', function () {
+
+  beforeEach(function () {
+    this.autoSave = require('./../index')
+  })
+
+  afterEach(function (cb) {
+    delete require.cache[require.resolve('./../index')]
+
+    fs.unlink(FILE, (err) => {
+      if(err && err.code !== 'ENOENT') {
+        cb(err)
+      } else {
+        cb()
+      }
+    })
+  })
+
+  describe('default', function () {
+    it('should return the same proxy if a file is use multiple times', function () {
+      const a = this.autoSave(FILE)
+      const b = this.autoSave(FILE)
+
+      assert.strictEqual(a, b)
+    })
+
+    it('should return the same proxy for the same sub-object', function () {
+      const a = this.autoSave(FILE)
+      a.b = {}
+
+      assert.strictEqual(a.b, a.b)
+    })
+  })
+
+  describe('with onSave', function () {
+
+    beforeEach(function () {
+      this.onSave = functs()
+      this.obj = this.autoSave(FILE, this.onSave)
+    })
+
+    it('should call the onSave function when the object is saved', function (cb) {
+      this.onSave.add(() => {
+        const json = fs.readFileSync(FILE)
+
+        assert.deepEqual(this.obj, JSON.parse(json))
+        cb()
+      })
+
+      this.obj.hallo = 'asdf'
+    })
+  })
+
+  describe('without onSave', function () {
+
+    beforeEach(function () {
+      this.obj = this.autoSave(FILE)
+    })
+
+    it('should save until the next tick', function (cb) {
+      this.obj.hallo = 'foo'
+
+      setImmediate(() => {
+
+        const json = fs.readFileSync(FILE)
+
+        assert.deepEqual(this.obj, JSON.parse(json))
+        cb()
+      })
+    })
+  })
+})
